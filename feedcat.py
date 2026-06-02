b49dd1e9293fd4b95154f7e7deb628ff23876f15
dan-gheorghita <79468719+dan-gheorghita@users.noreply.github.com>
2026-06-02 10:14:59 +0300

    Add Python script

---
+++ feedcat.py
@@ -0,0 +1,11 @@
+print('''# A multi-line string containing a letter to Alice
+# The letter asks her to feed Eve's cat
+# The letter is signed by Bob
+Dear Alice,
+
+# The main request of the letter
+Can you feed Eve's cat this weekend?
+
+# Closing of the letter
+Sincerely,
+Bob''')
